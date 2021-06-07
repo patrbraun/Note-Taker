a2440ac67c8069c9f0c3571ea0eb4e6bd00b334a
Patrick Braun <prbraun@protonmail.com>
2021-06-07 16:40:49 -0500

    Can now get and display notes

--- db/store.js
+++ db/store.js
@@ -1,9 +1,45 @@
-//look up uuid (util.promisify)
+//look up uuid
+const util = require('util')
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
-function getNotes(){
-    // return read().then{
-    //     //parse notes to return them as parsed notes
-    // }
+//look up util.promisify
+const readA = util.promisify(fs.readFile);
+const writeA = util.promisify(fs.writeFile);
+
+class Store{
+    async getNotes(){
+        console.log("Get notes");
+        const notes = await readA('db/db.json', JSON.stringify());
+        let result = [];
+        try{
+            result = JSON.parse(notes);
+            console.log("Get notes response: " + result);
+        }
+        catch(err){
+            console.log("No notes found");
+            result = [];
+        }
+        return result;
+
+        // readA('db/db.json', JSON.stringify())
+        // .then(notes => {
+        //     console.log([].concat(JSON.parse(notes)));
+        //     return [].concat(JSON.parse(notes));
+        // })
+        // .catch(err => {
+        //     console.error(err);
+        //     return [];
+        // })
+    }
+
+    setNotes(note){
+     //TODO
+    }
+
+    deleteNotes(id){
+        //TODO
+    }
 }
 
-module.exports = getNotes;
+module.exports = new Store();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,13 +3,24 @@ const store = require('../db/store');
 
 //create a route that responds with all notes coming from the data base
 router.get('/notes', (req, res) => {
+    console.log("router.get /notes");
     store.getNotes()
-    .then((notes) => {
-        return res.json(notes);
-    })
-    //.catch((err) => res.status(500).json(err));
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err));
+    
+    // return res.json([
+    //     {
+    //         "title":"Test Title",
+    //         "text":"Test text"
+    //     }
+    // ]);
 });
 
-
+router.post('/notes', (req, res) => {
+    console.log("router.post /notes");
+    store.setNotes(req.body)
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err));
+});
 
 module.exports = router;
